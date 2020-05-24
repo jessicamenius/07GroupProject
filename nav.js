9e77c09b091a1062f07ca56919729598bd57f49c
Jessica <jessicamenius@gmail.com>
2020-05-24 01:22:43 -0500

    editing dropdown for my games only

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".navbar").html(`    
-  <a class="navbar-brand" href=".././Index/index.html">Games App</a>
+  <a class="navbar-brand" href=".././index/index.html">Games App</a>
 <button
   class="navbar-toggler"
   type="button"
@@ -16,7 +16,7 @@ $(document).ready(function () {
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href=".././Index/index.html"
+      <a class="nav-link" href=".././index/index.html"
         >Home <span class="sr-only"></span></a
       >
     </li>
@@ -33,15 +33,7 @@ $(document).ready(function () {
         Games
       </a>
       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="../warcardgame/war.html">War</a>
-        <a class="dropdown-item" href="../memorygame/memorygame.html"
-          >Memory</a
-        >
         <a class="dropdown-item" href="../trivia/trivia.html">Trivia</a>
-        <a class="dropdown-item" href="../Snake/snake.html">Snake</a>
-        <a class="dropdown-item" href="../reactiongame/reaction.html"
-          >Reaction</a
-        >
         <a class="dropdown-item" href="../tictactoe/tictactoe.html"
           >Tic Tac Toe</a
         >
